2e09412d4c906aa47b152fc2de7d8adf980d8741
Danielwhyte <danwhy@gmail.com>
2018-08-21 11:32:01 +0100

    removes unused imports, fixes console warnings

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import handleFetch from '../helpers/handleFetch.js';
 
 class Login extends Component {
